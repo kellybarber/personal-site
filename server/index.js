b46b16de05d65d676095535226c2bcbae0abd444
kellybarber <kelly.james.barber@gmail.com>
2018-07-26 10:56:34 -0400

    Changed port distinction on server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const path = require('path')
 const publicPath = path.join(__dirname, '..', 'public')
 
@@ -10,4 +10,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.listen(port, () => console.log('Server is running on port 3000'))
+app.listen(port, () => console.log(`Server is running on port ${port}` ))
