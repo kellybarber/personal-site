1709ab6e342992558a64d4fd87a5b8c73cd91b28
kellybarber <kelly.james.barber@gmail.com>
2018-08-05 16:27:31 -0400

    Added new style folders including animations and created nav element

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <div className="header">
     <div className="header__cover">
-      
+      <h1 className="heading-primary">Kelly Barber</h1>
     </div>
   </div>
 )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Header from '../components/Header'
+import Navigation from '../components/Navigation'
 
 export default () => (
   <div>
+    <Navigation/>
     <Header/>
   </div>
 )
