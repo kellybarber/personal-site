9e9a3ecd756a42151397369d2be9941915f7fbb3
Kelly Barber <Kelly@Kellys-MacBook-Pro.local>
2018-09-12 11:35:31 -0400

    Created animation for sub-header

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,7 @@
 import React from 'react'
-import content from '../helpers/content'
 
 export default () => (
   <div className="about">
-    <div className="about__header--box">
-      <h2 className="about__header">Web <br/> Developer</h2>
-    </div>
-    <div className="about__content--box">
-      <p className="about__content">
-        {content.about}
-      </p>
-    </div>
+
   </div>
 )
