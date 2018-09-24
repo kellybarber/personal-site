186454452965bf4eddd792bdc84bcc6028d4e2d9
Kelly Barber <kelly.james.barber@gmail.com>
2018-09-24 16:57:26 -0400

    Added new fonts and permanent link to cover photo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Navigation = () => (
-  <div className='navigation'>
-    <input className='navigation__checkbox' type='checkbox' id='nav-toggle'/>
-    <label className='navigation__button' htmlFor="nav-toggle">
-      <span className="navigation__icon"></span>
-    </label>
-    <nav className='navigation__nav'>
-      <div className='navigation__list'>
-        <Link className='navigation__link' to='/'>Home</Link>
-        <Link className='navigation__link' to='/resume'>Resume</Link>
-        <Link className='navigation__link' to='/blog'>Blog</Link>
-      </div>
-    </nav>
-  </div>
-)
+const Navigation = () => {
+  return (
+    <div className='navigation'>
+      <input className='navigation__checkbox' type='checkbox' id='nav-toggle'/>
+      <label className='navigation__button' htmlFor="nav-toggle">
+        <span className="navigation__icon"></span>
+      </label>
+      <nav className='navigation__nav'>
+        <div className='navigation__list'>
+          <Link className='navigation__link' to='/'>Home</Link>
+          <Link className='navigation__link' to='/resume'>Resume</Link>
+          <Link className='navigation__link' to='/blog'>Blog</Link>
+        </div>
+      </nav>
+    </div>
+  )
+}
 
 export default Navigation
