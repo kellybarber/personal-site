7bdda909e7c02c70d9e3cdbd47292b63e5e22008
Kelly Barber <kelly.james.barber@gmail.com>
2018-09-26 11:14:04 -0400

    Added about section and created close button for modal

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default () => (
   <div className='about'>
-    <div className='about-title'></div>
+    <div className='about-title'>
+      <h1>about</h1>
+    </div>
     <div className='about-text'></div>
   </div>
 )
--- src/components/WorkModal.js
+++ src/components/WorkModal.js
@@ -6,6 +6,7 @@ const WorkModal = ({ showModal, modalInfo, handleCloseModal }) => (
   <div className={`work-modal ${showModal ? 'work-modal__show' : 'work-modal__hide'}`}>
     <div className='work-modal__background' onClick={handleCloseModal}></div>
     <div className='work-modal__content'>
+      <button className='work-modal__close' onClick={handleCloseModal}>&times;</button>
       <WorkModalText text={modalInfo.text}/>
       <WorkModalGallery photos={modalInfo.photos}/>
     </div>
