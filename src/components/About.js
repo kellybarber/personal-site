60a2d5f7ac9bae5cc6c4aedc407743dca6e3e0fe
Kelly Barber <kelly.james.barber@gmail.com>
2018-09-29 14:52:53 -0400

    Added some base text to about section

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,9 @@ export default () => (
         web apps. I have experience drafting themes and layouts and implementing them with CSS, as well as 
         experience building out complex modern front-end systems with JavaScript.
       </p>
+      <div className='about-social'>
+
+      </div>
     </div>
   </div>
 )
