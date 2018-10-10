58af9f92db60c775e46e7186530aa3a18508cd42
Kelly Barber <kelly.james.barber@gmail.com>
2018-10-10 15:33:20 -0400

    Updated resume pdf

--- src/components/ResumePDF.js
+++ src/components/ResumePDF.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default () => (
-  <embed className='resume__pdf' src='https://res.cloudinary.com/dwf5gbyud/image/upload/v1536771667/Personal%20Site%20Assets/Web_Developer_Resume.pdf'/>
+  <embed className='resume__pdf' src='https://res.cloudinary.com/dwf5gbyud/image/upload/v1539199956/Personal%20Site%20Assets/Web_Developer_Resume_mk2.pdf'/>
 )
