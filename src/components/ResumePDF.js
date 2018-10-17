f6f41fc1e8e44990d62419b03f24f75e5bf014c9
Kelly Barber <kelly.james.barber@gmail.com>
2018-10-17 09:37:13 -0400

    Updated resume with typo fixed

--- src/components/ResumePDF.js
+++ src/components/ResumePDF.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default () => (
-  <embed className='resume__pdf' src='https://res.cloudinary.com/dwf5gbyud/image/upload/v1539199956/Personal%20Site%20Assets/Web_Developer_Resume_mk2.pdf'/>
+  <embed className='resume__pdf' src='https://res.cloudinary.com/dwf5gbyud/image/upload/v1539783291/Personal%20Site%20Assets/Web_Developer_Resume_mk2.pdf'/>
 )
